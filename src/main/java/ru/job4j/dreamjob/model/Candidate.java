8751817973bf6e7bea2f5fca63f4ba6451f1ccca
aseldom <a.seldom@gmail.com>
2023-10-09 12:43:36 +0600

    7. HTML Link [#504843]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -6,15 +6,15 @@ import java.util.Objects;
 public class Candidate {
     private int id;
 
-    private String title;
+    private String name;
 
     private String description;
 
     private LocalDateTime creationDate = LocalDateTime.now();
 
-    public Candidate(int id, String title) {
+    public Candidate(int id, String name) {
         this.id = id;
-        this.title = title;
+        this.name = name;
     }
 
     public int getId() {
@@ -25,12 +25,12 @@ public class Candidate {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -42,7 +42,7 @@ public class MemoryCandidateRepository implements CandidateRepository {
     @Override
     public boolean update(Candidate candidate) {
         return candidates.computeIfPresent(candidate.getId(), (id, oldCandidate) ->
-                new Candidate(oldCandidate.getId(), candidate.getTitle())) != null;
+                new Candidate(oldCandidate.getId(), candidate.getName())) != null;
     }
 
     @Override
